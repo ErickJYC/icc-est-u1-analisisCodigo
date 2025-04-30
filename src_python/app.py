80a185543ba3f5eed2173e31cd4529012959c21e
Erick Yunga <eyungac3@est.ups.edu.ec>
2025-04-30 09:19:04 -0500

    repaso python

---
+++ src_python/app.py
@@ -0,0 +1,9 @@
+# import benchamarking as Ben
+from benchamarking import Benchmarking
+
+
+
+if __name__ == "__main__":
+    print("Funciona")
+    Benchmarking()
+    
---
+++ src_python/benchamarking.py
@@ -0,0 +1,43 @@
+import random 
+import time
+from  metodos_Ordenamiento import MetodosOrdenamiento
+
+
+class Benchmarking:
+    def __init__(self):
+        print('Bench inicializado')
+        self.mOordenamiento = MetodosOrdenamiento()
+        arreglo = self.bulli_arreglo(50000)
+
+        tarea = lambda: self.mOordenamiento.sortByBubble(arreglo)
+        tiempoMillis = self.contar_con_current_time_milles(tarea)
+        tiempoNano = self.contar_con_nano_time(tarea)
+
+        print(f'tiempoMillos{tiempoMillis}')
+        print(f'tiempoNano{tiempoNano}')
+
+
+
+
+    def bulli_arreglo(self, tam):
+        array = []
+        for i in range(tam):
+            numero = random.randint(0,99999)
+            array.append(numero)
+        return array
+    
+    # ejecutar tarea Tarea()
+    
+
+    def contar_con_current_time_milles(self, tarea):
+        inicio = time.time()
+        tarea()
+        fin = time.time()
+        return (fin - inicio)
+    
+    def contar_con_nano_time(self, tarea):
+         inicio = time.time_ns()
+         tarea()
+         fin = time.time_ns()
+         return (fin - inicio) / 1_000_000_000.0
+         
